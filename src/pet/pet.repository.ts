be61ff5b74b7a1ad6b335d93e09246564937e316
zequiel <zequiel73@gmail.com>
2024-04-10 22:12:27 -0300

    feat:funcao deleta pet

--- src/pet/pet.repository.ts
+++ src/pet/pet.repository.ts
@@ -6,11 +6,11 @@ import { Model } from "mongoose";
 
 @Injectable()
 export default class PetRepository implements IPetRepository {
-    
+
     constructor(
         @InjectModel(Pet.name)
         private readonly petModel: Model<Pet>,
-      ) {}
+    ) { }
 
     async getById(id: string): Promise<Pet> {
         return await this.petModel.findById(id)
@@ -23,5 +23,20 @@ export default class PetRepository implements IPetRepository {
             updatedAt: new Date()
         })
     }
-    
+
+    async updateById(data: Partial<Pet>): Promise<void> {
+        await this.petModel.updateOne(
+            {
+                _id: data._id
+            }, {
+            ...data,
+            updatedAt: new Date()
+        }
+        )
+    }
+
+    async deleteById(id: string): Promise<void> {
+        await this.petModel.findByIdAndDelete(id)
+    }
+
 }
